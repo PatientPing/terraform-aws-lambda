008bfe04dfb80b2e120bdc2bb4632e20ae008f13
jeid <jeid@hbu.co>
2023-03-14 09:38:02 -0400

    PNGDAT-65
    - Create codebuild job for lambdas that just runs unit tests and on all branches

--- lambda_function/ci.tf
+++ lambda_function/ci.tf
@@ -113,3 +113,55 @@ resource "aws_codebuild_webhook" "lambda" {
     }
   }
 }
+
+resource "aws_codebuild_project" "lambda_unit" {
+  count = var.github_url == "" ? 0 : 1
+
+  name          = var.function_name
+  build_timeout = var.build_timeout
+  service_role  = aws_iam_role.codebuild[0].arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    environment_variable {
+      name = "run_unit"
+      value = var.codebuild_can_run_integration_test
+    }
+    environment_variable {
+      name  = "lambda_function_name"
+      value = var.function_name
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = var.github_url
+    git_clone_depth = 1
+
+    auth {
+      type     = "OAUTH"
+      resource = var.codebuild_credential_arn == "" ? "arn:aws:codebuild:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:token/github" : var.codebuild_credential_arn
+    }
+  }
+}
+
+resource "aws_codebuild_webhook" "lambda" {
+  count = var.github_url == "" ? 0 : 1
+
+  project_name = aws_codebuild_project.lambda_unit[0].name
+
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+  }
+}
+
