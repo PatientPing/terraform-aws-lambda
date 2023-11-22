bbf7e6e9a4133659ddd61824b9bd6dc9a317e0cc
smallesh90 <smallesh@bamboohealth.com>
2023-11-22 12:16:07 -0800

    update

--- lambda_function/test_trigger.tf
+++ lambda_function/test_trigger.tf
@@ -49,15 +49,6 @@ resource "aws_iam_role_policy" "unit_test_codebuild" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "ssm:GetParameter*"
-        ],
-        Resource = [
-          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/cloudeng/infra/github/token"
-        ]
       }
     ]
   })
