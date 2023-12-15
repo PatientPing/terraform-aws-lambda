76e3b212e1c2cbdc510b927e5b8006fd19cd302c
Suhas Mallesh <smallesh@bamboohealth.com>
2023-12-15 07:50:46 -0800

    Add unit_test_trigger access to parameter (#30)

--- lambda_function/unit_test_trigger.tf
+++ lambda_function/unit_test_trigger.tf
@@ -49,6 +49,15 @@ resource "aws_iam_role_policy" "unit_test_codebuild" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ssm:GetParameter*"
+        ],
+        Resource = [
+          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/cloudeng/infra/github/token"
+        ]
       }
     ]
   })
