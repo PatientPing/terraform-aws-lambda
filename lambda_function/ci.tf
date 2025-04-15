ebb1fc560011ebf415659bd353cd6fb2b83bc60d
tyleragostinobamboo <tagostino@hbu.co>
2025-04-15 09:39:48 -0400

    fix trailing bracket

--- lambda_function/ci.tf
+++ lambda_function/ci.tf
@@ -86,7 +86,6 @@ data "aws_iam_policy_document" "policy" {
         "ssm:GetParameter*"
       ]
       resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.github_token_path}"]
-      ]
     }
   }
 
