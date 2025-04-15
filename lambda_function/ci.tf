330f535ee809bbad0135d82475aac1cb9bdb6cd6
tyleragostinobamboo <tagostino@hbu.co>
2025-04-15 09:30:30 -0400

    fix iam policy for parameter path

--- lambda_function/ci.tf
+++ lambda_function/ci.tf
@@ -85,7 +85,7 @@ data "aws_iam_policy_document" "policy" {
       actions = [
         "ssm:GetParameter*"
       ]
-      resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/service/shared/*"
+      resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.github_token_path}"]
       ]
     }
   }
