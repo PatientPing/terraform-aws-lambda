cbffd5d4c35e9fe49322ca1294b350758bf9ad22
bugs404 <jeid@bamboohealth.com>
2026-04-15 09:55:50 -0400

    PNGDAT-3777 add provisioned concurrency support

--- lambda_function/outputs.tf
+++ lambda_function/outputs.tf
@@ -14,6 +14,26 @@ output "invoke_arn" {
   value = aws_lambda_function.lambda.invoke_arn
 }
 
+output "version" {
+  value = aws_lambda_function.lambda.version
+}
+
+output "qualified_arn" {
+  value = aws_lambda_function.lambda.qualified_arn
+}
+
+output "alias_name" {
+  value = try(aws_lambda_alias.lambda[0].name, null)
+}
+
+output "alias_arn" {
+  value = try(aws_lambda_alias.lambda[0].arn, null)
+}
+
+output "alias_invoke_arn" {
+  value = try(aws_lambda_alias.lambda[0].invoke_arn, null)
+}
+
 output "codebuild_role" {
   value = aws_iam_role.codebuild
 }
