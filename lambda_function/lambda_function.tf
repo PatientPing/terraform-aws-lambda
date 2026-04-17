ae11f323f2d5f64c09ab1db0722b8d2768f38a17
Tyler Agostino <tagostino@bamboohealth.com>
2026-04-17 15:51:45 -0400

    sumo urls and remove bad gh_sha argument

--- lambda_function/lambda_function.tf
+++ lambda_function/lambda_function.tf
@@ -1,5 +1,28 @@
 locals {
-  log_group_name = var.log_group_name == null ? "/aws/lambda/${var.function_name}" : var.log_group_name
+  log_group_name   = var.log_group_name == null ? "/aws/lambda/${var.function_name}" : var.log_group_name
+  sumo_url_nonprod = "https://endpoint4.collection.sumologic.com/receiver/v1/http/ZaVnC4dhaV0ao2ExQYwmLW4hoL8VbQDQWdmhj8Ojl7HlhSVOTgSlruBYklxlsqldlUyKLu3XXcSaOWTLcD_IxeI3VLHwhqttwDem8whV0JsB53slszdDUQ=="
+  sumo_url_prod    = "https://endpoint4.collection.sumologic.com/receiver/v1/http/ZaVnC4dhaV0aoMJW0JPZTDCAbMiIdthg3-v7oZZxA_1ehOVR7kSNMipXTIN1ArpkKe2vl70z9RXd5_wYFaGiLJZ2IqLKQIz25qU3sWQxuq9C7tHCJ0xm9w=="
+  sumo_endpoint_url_mapping = {
+    "499407627146" : local.sumo_url_prod,
+    "139270343000" : local.sumo_url_prod,
+    "085651866557" : local.sumo_url_nonprod,
+    "225726765411" : local.sumo_url_nonprod,
+    "334700497158" : local.sumo_url_nonprod,
+    "934421957606" : local.sumo_url_nonprod,
+    "424344727374" : local.sumo_url_nonprod,
+    "755176673017" : local.sumo_url_nonprod,
+    "389016375993" : local.sumo_url_prod,
+    "691648844592" : local.sumo_url_prod,
+    "651305130338" : local.sumo_url_prod,
+    "477845385016" : local.sumo_url_prod,
+    "066045530129" : local.sumo_url_prod,
+    "523930181136" : local.sumo_url_prod,
+    "551052347425" : local.sumo_url_nonprod,
+    "011516987840" : local.sumo_url_prod,
+    "614143157168" : local.sumo_url_prod,
+    "050919298421" : local.sumo_url_prod,
+    "079655036558" : local.sumo_url_nonprod,
+  }
 }
 
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
@@ -42,7 +65,7 @@ resource "aws_lambda_function" "lambda" {
 
   environment {
     variables = merge(var.environment_variables, {
-      GH_SHA = data.aws_region.current.name
+      SUMO_HTTP_ENDPOINT = lookup(local.sumo_endpoint_url_mapping, data.aws_caller_identity.current.account_id, null)
     })
   }
 
